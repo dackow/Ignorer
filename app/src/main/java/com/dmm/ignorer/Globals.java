70669477f3589d8c6c3064e32c22208813f34f85
dackow <dackow@gmail.com>
2015-06-30 09:44:40 +0200

    Notification part 1

---
+++ app/src/main/java/com/dmm/ignorer/Globals.java
@@ -0,0 +1,15 @@
+package com.dmm.ignorer;
+
+/**
+ * Created by waldekd on 2015-06-30.
+ */
+public class Globals {
+    //changing this make sure that AndroidManifest.xml will be changed as well
+    public static final String ACTION_SHOW_START_ACTIVITY = "com.dmm.ignorer.action.SHOW_START_ACTIVITY";
+
+    //request codes
+    public static final int REQ_CODE_SHOW_START_ACTIVITY = 0;
+
+    //notification codes
+    public static final int NOTIF_CODE = 0;
+}
--- app/src/main/java/com/dmm/ignorer/StartForm.java
+++ app/src/main/java/com/dmm/ignorer/StartForm.java
@@ -1,29 +1,83 @@
 package com.dmm.ignorer;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 
 public class StartForm extends Activity {
+    NotificationManager notificationManager = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_form);
+
+        if (getIntent() != null) {
+            cancelNotification();
+        }
     }
 
-    public void onClick(View v){
-       switch(v.getId()){
-           case R.id.btnStart:
-
-               break;
-       }
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnStart:
+                startApp();
+                break;
+            case R.id.btnStop:
+                stopApp();
+                break;
+        }
     }
 
+    private void startApp() {
+        //register receiver
+        //...
+        if (notificationManager == null) {
+            notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        Notification notification = buildNotification();
+        notificationManager.notify(Globals.NOTIF_CODE, notification);
+        finish();
+    }
+
+    private void stopApp() {
+        //unregister receiver
+        cancelNotification();
+        finish();
+    }
+
+
+    private Notification buildNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.drawable.phone);
+        builder.setContentTitle(getResources().getString(R.string.app_name));
+        builder.setContentText(getResources().getString(R.string.notif_text));
+        Intent intent2fire = new Intent(this, StartForm.class);
+        intent2fire.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        android.support.v4.app.TaskStackBuilder stackBuilder = android.support.v4.app.TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(StartForm.class);
+        stackBuilder.addNextIntent(intent2fire);
+
+        PendingIntent resultIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);//createPendingResult(Globals.REQ_CODE_SHOW_START_ACTIVITY, intent2fire, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(resultIntent);
+        return builder.build();
+    }
+
+    private void cancelNotification() {
+        if (notificationManager == null) {
+            notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        notificationManager.cancel(Globals.NOTIF_CODE);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
